3eef294d45ff1f754b0e3e2a016d40d2c2db786b
Tauqeer Ahmed Shakir <ishop@iShops-MacBook-Pro.local>
2024-11-16 12:09:25 +0500

    First commit

---
+++ BodyMassIndex/main.py
@@ -0,0 +1,11 @@
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+# bmi = weight / height^2
+
+bmi = int(float(weight) / float(height)**2)
+
+print(bmi)
+
+
+
