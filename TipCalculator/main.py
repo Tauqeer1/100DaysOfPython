b53618e3fb3dd81e35ba876e7778848019329da3
Tauqeer Ahmed Shakir <ishop@iShops-MacBook-Pro.local>
2024-11-16 13:05:51 +0500

    Challenge completed tip calculator

--- TipCalculator/main.py
+++ TipCalculator/main.py
@@ -1,4 +1,13 @@
-num = 3 * 3 + 3 / 3 - 3 # 7.0
-num1 = 3 * (3 + 3) / 3 - 3 # 3.0
+print("Welcome to the tip calculator.")
+total_bill = float(input("What was your total bill? $"))
+tip_percentage = float(input("What percentage tip would you like to give? 10, 12, or 15? "))
+no_of_people = int(input("How many people to split the bill? "))
 
-print(num1)
+
+total_tip_amount = total_bill * (tip_percentage / 100)
+
+split_amount = round((total_bill + total_tip_amount) / no_of_people, 2)
+
+message = f"Each person should pay: ${split_amount}"
+
+print(message)
