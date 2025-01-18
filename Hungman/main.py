8670ccc522af203f0b7744c17d4e7a0200400a6d
Tauqeer Ahmed Shakir <ishop@iShops-MacBook-Pro.local>
2025-01-18 13:41:33 +0500

    changes

--- Hungman/main.py
+++ Hungman/main.py
@@ -1,25 +1,40 @@
 import random
+from hungman_stages import stages
+from hungman_words import word_list
 
-word_list = ["ardvark", "baboon", "camel"]
+# word_list = ["ardvark", "baboon", "camel"]
 
-# 1st way
-# chosen_word = random.choice(word_list)
-# 2nd way
-chosen_word = word_list[random.randrange(0, len(word_list))]
+chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
-print(f"Chosen Word: {chosen_word}")
+# print(f"Chosen Word: {chosen_word}")
 
 display = []
-
 for _ in range(word_length):
     display.append("_")
 
-guess = input("Guess a letter: ").lower()
+end_of_game = False
+lives = 6
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
-print(f"Letter: {guess}")
+    # print(f"Guessed Letter: {guess}")
+    if guess in display:
+        print(f"You've already guessed {guess}")
+    elif guess in chosen_word:
+        for i in range(word_length):
+            letter = chosen_word[i]
+            if guess == letter:
+                display[i] = letter
+    else:
+        lives -= 1
+        print(f"You guessed {guess}, that's not in the word. You lose a life")
+        print(stages[lives])
 
-for i in range(word_length):
-    letter = chosen_word[i]
-    if guess == letter:
-        display[i] = letter
-print(display)
+    print(' '.join(display))
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win")
+    elif lives == 0:
+        end_of_game = True
+        print("You lose")
