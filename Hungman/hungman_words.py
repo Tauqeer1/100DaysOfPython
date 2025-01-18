d03f590d75cb522aa8552c75ff7ee723675aa10a
Tauqeer Ahmed Shakir <tauqeer.shakir@yahoo.com>
2025-01-18 13:41:33 +0500

    changes

---
+++ Hungman/hungman_words.py
@@ -0,0 +1,12 @@
+word_list = [
+    "apple", "banana", "cherry", "grape", "mango", "orange", "peach", "pear", "plum", "kiwi",
+    "tomato", "avocado", "blueberry", "strawberry", "raspberry", "blackberry", "pineapple", "coconut", "papaya", "watermelon",
+    "elephant", "giraffe", "zebra", "lion", "tiger", "cheetah", "panda", "koala", "kangaroo", "camel",
+    "butterfly", "dragonfly", "grasshopper", "ant", "beetle", "cricket", "mosquito", "spider", "scorpion", "fly",
+    "chess", "puzzle", "board", "cards", "dice", "dominoes", "monopoly", "scrabble", "checkers", "jigsaw",
+    "python", "javascript", "typescript", "angular", "node", "django", "flask", "react", "bootstrap", "jquery",
+    "guitar", "piano", "violin", "drums", "flute", "trumpet", "saxophone", "clarinet", "accordion", "cello",
+    "mountain", "valley", "river", "ocean", "forest", "desert", "prairie", "tundra", "island", "plateau",
+    "coffee", "tea", "juice", "milk", "water", "soda", "smoothie", "latte", "espresso", "mocha",
+    "circle", "square", "triangle", "rectangle", "hexagon", "octagon", "pentagon", "ellipse", "rhombus", "parallelogram"
+]
