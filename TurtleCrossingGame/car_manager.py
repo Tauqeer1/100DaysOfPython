64991dbcac33d8b247625b2ea27e7c1370686192
Tauqeer <tauqeer.shakir@yahoo.com>
2025-04-30 19:52:05 +0500

    created random cars

--- TurtleCrossingGame/car_manager.py
+++ TurtleCrossingGame/car_manager.py
@@ -1,3 +1,5 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
@@ -5,4 +7,26 @@ MOVE_INCREMENT = 10
 # Generate all the random cars
 class CarManager:
     def __init__(self):
-        pass
+        self.all_cars = []
+        # self.shape("square")
+        # self.shapesize(stretch_wid=1, stretch_len=2)
+        # self.setheading(180)
+        # self.penup()
+        # self.color(random.choice(COLORS))
+        # self.goto(280, random.randint(-260, 260))
+
+
+    def create_car(self):
+        random_chance = random.randint(1, 10)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_len=2, stretch_wid=1)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
+
+    def move_car(self):
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
--- TurtleCrossingGame/main.py
+++ TurtleCrossingGame/main.py
@@ -9,13 +9,17 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
-
+car_manager = CarManager()
 screen.onkeypress(player.move_up, "Up")
 screen.listen()
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
+    car_manager.create_car()
+    car_manager.move_car()
+
+
     screen.update()
 
 
