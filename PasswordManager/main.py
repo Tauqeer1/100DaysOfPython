d4cbfd8f2cc50f9f86b715a4e518a60bcd10bac4
Tauqeer <tauqeer.shakir@yahoo.com>
2025-05-06 19:49:40 +0500

    Generate Password functionality completed

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -7,6 +8,26 @@ WHITE = "#ffffff"
 BLACK = "#000000"
 GRAY = "#635d5c"
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = [random.choice(letters) for _ in range(nr_letters)]
+    password_list += [random.choice(symbols) for _ in range(nr_symbols)]
+    password_list += [random.choice(numbers) for _ in range(nr_numbers)]
+
+    random.shuffle(password_list)
+    password = ''.join(password_list)
+
+    password_input.delete(0, END)
+    password_input.insert(0, password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -25,7 +46,6 @@ def save_data():
         is_ok = messagebox.askokcancel(title=website_value,
                                        message=f"These are the details entered: \n Email: {email_value} \n "
                                                f"Password: {password_value} \n Is it ok to save ?")
-        print(is_ok)
         if is_ok:
             text_to_write = f"{website_value} | {email_value} | {password_value}\n"
             write_to_file("data.txt", text_to_write)
@@ -66,10 +86,10 @@ email_input.grid(row=2, column=1, columnspan=2)
 password_label = Label(text="Password:", bg=WHITE, fg=BLACK, pady=5)
 password_label.grid(row=3, column=0)
 
-password_input = Entry(width=21, bg=WHITE, fg=BLACK, show="*", relief="flat", highlightcolor=GRAY, highlightthickness=1, insertbackground=BLACK)
+password_input = Entry(width=21, bg=WHITE, fg=BLACK, relief="flat", highlightcolor=GRAY, highlightthickness=1, insertbackground=BLACK)
 password_input.grid(row=3, column=1)
 
-password_generate_button = Button(text="Generate Password", highlightbackground=WHITE, width=11, relief="raised")
+password_generate_button = Button(text="Generate Password", highlightbackground=WHITE, width=11, relief="raised", command=generate_password)
 password_generate_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=36, highlightbackground=WHITE, relief="raised", command=save_data)
