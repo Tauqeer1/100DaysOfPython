2fa739a2460df23863c328b0ca1076f4a44dde4d
Tauqeer <tauqeer.shakir@yahoo.com>
2025-05-09 19:40:20 +0500

    fix the issue of question text using html unescape method

--- QuizzlerAppGUI/data.py
+++ QuizzlerAppGUI/data.py
@@ -7,7 +7,6 @@ parameters = {
 response = requests.get('https://opentdb.com/api.php', params=parameters)
 response.raise_for_status()
 data = response.json()
-print(data['results'])
 
 question_data = data['results']
 
