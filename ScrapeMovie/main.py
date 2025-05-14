bb744c63fbd9b5f91e1b20b9797d1b66206a8619
Tauqeer <tauqeer.shakir@yahoo.com>
2025-05-14 23:26:09 +0500

    Scrape the title from html

--- ScrapeMovie/main.py
+++ ScrapeMovie/main.py
@@ -4,7 +4,24 @@ import requests
 response = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
 response.raise_for_status()
 
-movies_page = response.text
+html_content = response.text
 
-soup = BeautifulSoup(movies_page, 'html.parser')
-print(soup.prettify())
+soup = BeautifulSoup(html_content, 'html.parser')
+
+movie_title = soup.find_all('span', class_='content_content__i0P3p')
+# for title in movie_title:
+#     if title.find('h2') and title.find('h2').find('strong'):
+#         title_text = title.find('h2').find('strong').get_text()
+#         print(title_text)
+
+title_text = [title.find('h2').find('strong').get_text()
+              for title in movie_title
+              if title.find('h2') and title.find('h2').find('strong')]
+
+
+
+
+
+
+
+
