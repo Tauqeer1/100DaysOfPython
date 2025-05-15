af33ad097585902e5a044c060b189143805231a1
Tauqeer <tauqeer.shakir@yahoo.com>
2025-05-15 17:28:21 +0500

    Extract the title text from html tag

--- MusicalTimeMachine/main.py
+++ MusicalTimeMachine/main.py
@@ -3,9 +3,10 @@ from bs4 import BeautifulSoup
 
 BILLBOARD_BASE_URL = "https://www.billboard.com/charts/hot-100"
 
-user_input_date = input("Which year do you want to travel to? Type the date in this format: YYYY-MM-DD: ")
+# user_input_date = input("Which year do you want to travel to? Type the date in this format: YYYY-MM-DD: ")
 
-billboard_url = f"{BILLBOARD_BASE_URL}/{user_input_date}"
+# billboard_url = f"{BILLBOARD_BASE_URL}/{user_input_date}"
+billboard_url = f"{BILLBOARD_BASE_URL}/1992-12-22"
 
 
 billboard_response = requests.get(billboard_url,
@@ -15,4 +16,7 @@ billboard_html_content = billboard_response.text
 
 soup = BeautifulSoup(billboard_html_content, "html.parser")
 
-print(soup.prettify())
+h3_tags = soup.find_all("h3", id="title-of-a-story", class_="a-no-trucate")
+
+song_title = [h3_tag.get_text().strip() for h3_tag in h3_tags]
+print(song_title)
