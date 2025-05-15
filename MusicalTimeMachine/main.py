148f6276ab34dbdc0a28ec8e6a42e359fbec0243
Tauqeer <tauqeer.shakir@yahoo.com>
2025-05-15 23:41:07 +0500

    Task completed

--- MusicalTimeMachine/main.py
+++ MusicalTimeMachine/main.py
@@ -2,18 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from pprint import pprint
 
 BILLBOARD_BASE_URL = "https://www.billboard.com/charts/hot-100"
 SPOTIFY_CLIENT_ID = "cc2e23f16916454db2b6763b6bb64b42"
 SPOTIFY_CLIENT_SECRET = "6048ca3e35984b069dfb0e08d621b47f"
 SPOTIFY_REDIRECT_URI = "https://example.com/callback"
 
-# user_input_date = input("Which year do you want to travel to? Type the date in this format: YYYY-MM-DD: ")
+user_input_date = input("Which year do you want to travel to? Type the date in this format: YYYY-MM-DD: ")
 
-# billboard_url = f"{BILLBOARD_BASE_URL}/{user_input_date}"
-billboard_url = f"{BILLBOARD_BASE_URL}/1992-12-22"
-
+billboard_url = f"{BILLBOARD_BASE_URL}/{user_input_date}"
 
 billboard_response = requests.get(billboard_url,
                                   headers={"User-Agent": "Chrome/136.0.0.0"})
@@ -31,31 +28,36 @@ list_of_scope = ['user-read-email','user-follow-read', 'playlist-read-private',
                  'playlist-modify-public']
 SPOTIFY_SCOPES = ' '.join(list_of_scope)
 
-
+# Spotify Authentication
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,
                                                client_secret=SPOTIFY_CLIENT_SECRET,
                                                scope=SPOTIFY_SCOPES,
                                              redirect_uri=SPOTIFY_REDIRECT_URI))
 
-user_input_date = "1992-12-22"
 year = user_input_date.split("-")[0]
 
-# user_id = sp.current_user()['id']
-# playlist = sp.user_playlist_create(user=user_id,
-#                                    # name=f"{user_input_date} Billboard 100",
-#                                    name="Test1",
-#                                    public=True,
-#                                    collaborative=False)
-#
-# playlist_id = playlist['id']
-
+# Searching spotify to find the song by title
 song_uris = []
 for title in song_title:
-    searched_result = sp.search(q=f"track: {title} year: {year}", type="track", market=None, limit=1)
-    searched_metadata = searched_result['tracks']['items'][0]
-    song_uris.append(searched_metadata["uri"])
-    # data = {
-    #     'external_url': searched_metadata['external_urls']['spotify'],
-    #     'uri': searched_metadata['uri']
-    # }
-print(song_uris)
+    searched_result = sp.search(q=f"track: {title} year: {year}", type="track")
+    try:
+        uri = searched_result['tracks']['items'][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{title} doesn't exist in Spotify. Skipped.")
+
+
+user_id = sp.current_user()['id']
+# Creating a private playlist in spotify
+playlist = sp.user_playlist_create(user=user_id,
+                                   name=f"{user_input_date} Billboard 100",
+                                   public=False,
+                                   collaborative=False)
+
+playlist_id = playlist['id']
+# Add song to the playlist
+sp.playlist_add_items(playlist_id=playlist_id, items=song_uris)
+print('Tracks added successfully')
+
+
+
