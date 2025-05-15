3391e9f9c08b32344d692b4b306cb64e7cb400b0
Tauqeer <tauqeer.shakir@yahoo.com>
2025-05-15 17:10:18 +0500

    html scrapper added

--- MusicalTimeMachine/main.py
+++ MusicalTimeMachine/main.py
@@ -13,4 +13,6 @@ billboard_response = requests.get(billboard_url,
 billboard_response.raise_for_status()
 billboard_html_content = billboard_response.text
 
-print(billboard_html_content)
+soup = BeautifulSoup(billboard_html_content, "html.parser")
+
+print(soup.prettify())
