c63304275bb48742f9d8ea4387bdfdee29622adb
Tauqeer <tauqeer.shakir@yahoo.com>
2025-05-24 00:25:02 +0500

    guess number functionality completed

--- HigherLowerFlaskApp/server.py
+++ HigherLowerFlaskApp/server.py
@@ -3,16 +3,26 @@ import random
 
 app = Flask(__name__)
 
+# global random_number
 
 @app.route('/')
 def index():
-
-    random_number = random.randint(0, 9)
-
-
+    # global random_number
+    # random_number = random.randint(0, 9)
+    # print(random_number)
     return ('<p>Guess a number between 0 and 9</p>'
             '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif" />')
 
+@app.route('/guess/<int:number>')
+def guess(number):
+    random_number = random.randint(0, 9)
+    if number < random_number:
+        message = f"{number} is too low"
+    elif number > random_number:
+        message = f"{number} is too high"
+    else:
+        message = f"{number} is just right"
+    return message
 
 if __name__ == '__main__':
     app.run(debug=True)
