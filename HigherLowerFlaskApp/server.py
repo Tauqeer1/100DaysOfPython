7d883f082e6b59c011482e595a5fe233d7cc2c7e
Tauqeer <tauqeer.shakir@yahoo.com>
2025-05-24 00:08:06 +0500

    generate random number

--- HigherLowerFlaskApp/server.py
+++ HigherLowerFlaskApp/server.py
@@ -1,11 +1,15 @@
 from flask import Flask
-
+import random
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+
+    random_number = random.randint(0, 9)
+
+
     return ('<p>Guess a number between 0 and 9</p>'
             '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif" />')
 
