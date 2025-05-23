bb3b4e913427d4dc657575b72fdc9bc0c2a1dbd8
Tauqeer <tauqeer.shakir@yahoo.com>
2025-05-24 00:29:32 +0500

    task completed

--- HigherLowerFlaskApp/server.py
+++ HigherLowerFlaskApp/server.py
@@ -3,19 +3,19 @@ import random
 
 app = Flask(__name__)
 
-# global random_number
+global random_number
 
 @app.route('/')
 def index():
-    # global random_number
-    # random_number = random.randint(0, 9)
+    global random_number
+    random_number = random.randint(0, 9)
     # print(random_number)
     return ('<p>Guess a number between 0 and 9</p>'
             '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif" />')
 
 @app.route('/guess/<int:number>')
 def guess(number):
-    random_number = random.randint(0, 9)
+    # random_number = random.randint(0, 9)
     if number < random_number:
         message = f"{number} is too low"
     elif number > random_number:
